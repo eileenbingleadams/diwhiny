1b350184dfe0a407ae05e036a5d5c840b2451599
eileenbingleadams <57018644+eileenbingleadams@users.noreply.github.com>
2020-01-16 17:04:03 -0600

    Update again to photo path

--- wines.js
+++ wines.js
@@ -4,7 +4,7 @@ WINES.data = [
     {
         id:1,
         title:"Wine 1",
-        image: "assets/photos/meiomi-pinot-noir-2017.jpg",
+        image: "../assets/photos/meiomi-pinot-noir-2017.jpg",
         description: "description of this wine",
         variety: "pinotNoir",
         price: "10-20",
@@ -13,7 +13,7 @@ WINES.data = [
     {
         id:2,
         title: "Wine 2",
-        image: "assets/photos/kim-crawford-sav-blanc-2019.jpg",
+        image: "../assets/photos/kim-crawford-sav-blanc-2019.jpg",
         description: "description of this wine",
         variety: "sauvignonBlanc",
         price: "20-40",
@@ -22,7 +22,7 @@ WINES.data = [
     {
         id:3,
         title: "Wine 3",
-        image: "assets/photos/luc-belaire-luxe-rose.jpg",
+        image: "../assets/photos/luc-belaire-luxe-rose.jpg",
         description: "description of this wine",
         variety: "bubbly",
         price: "LT10",
@@ -31,7 +31,7 @@ WINES.data = [
     {
         id:4,
         title: "Wine 4",
-        image: "assets/photos/flowers-chardonnay-sonoma-coast-2017.jpg",
+        image: "../assets/photos/flowers-chardonnay-sonoma-coast-2017.jpg",
         description: "description of this wine",
         variety: "chardonnay",
         price: "10-20",

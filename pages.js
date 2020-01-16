2ec4f147656f8d03da744cd2564322f5b9ee10f7
eileenbingleadams <57018644+eileenbingleadams@users.noreply.github.com>
2020-01-16 17:34:55 -0600

    Fixing links

--- pages.js
+++ pages.js
@@ -4,7 +4,7 @@ PAGES.data = [
     {
         id:1,
         title:"Chewy Gingerbread Cookies",
-        href: "/projects/gingerbread-cookies/index.html",
+        href: "projects/gingerbread-cookies/index.html",
         image: "assets/photos/gingerbread_project.jpg",
         description:"Chewy not gooey. Why is this so difficult?",
         keywords: ["gingerbread","cookies","cookie","chewy","baking","baked goods","food"]
@@ -12,7 +12,7 @@ PAGES.data = [
     {
         id:2,
         title: "Homemade Chex Mix",
-        href: "/projects/homemade-chex-mix/index.html",
+        href: "projects/homemade-chex-mix/index.html",
         image: "assets/photos/chexmix_project.jpg",
         description: "Why buy at the grocery store when you can spend hours making this at home?",
         keywords: ["chex","mix","food","snacks","snack"]
